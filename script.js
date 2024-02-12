37ed27387cc6819ef0c2cf68d61125d1076ca4ec
salt2038 <53345566+salt2038@users.noreply.github.com>
2024-02-12 11:36:57 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,6 @@
 
   let faceArray = [];
   params.forEach(function(value,key){
-    if(key.includes("hood")){
       switch(value){
         case "5":
             faceArray.push("☆☆☆☆☆")
@@ -23,7 +22,6 @@
             faceArray.push("☆")
             break;
       }
-    }
   });
   replaceComment(faceArray);
 });
@@ -32,11 +30,7 @@ function replaceComment(faceArray){
     let comment = "<p>喜び： "+faceArray[0]+"</p>"+
                 "<p>悲しみ： "+faceArray[1]+"</p>"+
                 "<p>怒り： "+faceArray[2]+"</p>"+
-                "<p>驚き： "+faceArray[3]+"</p><br>"+
-                "<p>※以下の指標で☆が多い場合は、うまく分析が出来ていない可能性があります。</p>"+
-                "<p>画像の白さ： "+faceArray[4]+"</p>"+
-                "<p>画像のぼやけ： "+faceArray[5]+"</p>"+
-                "<p>帽子等の着用： "+faceArray[6]+"</p>";
+                "<p>驚き： "+faceArray[3]+"</p>"
     //console.log(comment)
     document.getElementById('comment').innerHTML = comment;
     document.getElementById('comment').style.padding = "0.5em 1em"
